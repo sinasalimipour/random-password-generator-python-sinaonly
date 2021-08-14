4e04c19eb3f40a7d79300f70c9f80cf8afcfbbd9
sinagun <38187655+sinagun@users.noreply.github.com>
2021-08-15 02:52:50 +0430

    0.3

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+
+from tkinter import *
+import tkinter
 import random
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()_+}{"
-while 1:
-    password_len = int(input("what length would you like to your password be:"))
-    password_count = int(input("how many password would you like :"))
-    for x in range(0,password_count):
-        password = ""
-        for x in range(0,password_len):
-            password_char = random.choice(chars)
-            password = password + password_char
-        print("here is your password",password)
+
+root = Tk()
+myLabel = Label(root, text="hello world")
+
+myLabel.pack()
+
+
+
+
+
+
+
+
+
+
+
+# #password-GENRATOR start
+# chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%^&*()_+}{"
+# while 1:
+#     password_len = int(input("what length would you like to your password be:"))
+#     password_count = int(input("how many password would you like :"))
+#     for x in range(0,password_count):
+#         password = ""
+#         for x in range(0,password_len):
+#             password_char = random.choice(chars)
+#             password = password + password_char
+#         print("here is your password",password)
+# #password-GENRATOR end
